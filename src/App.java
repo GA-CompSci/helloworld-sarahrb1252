90d987eba37db1d9859376dd515cec9fec3f18c3
sarahrb1252 <sarah.bowen@gilmour.org>
2025-08-26 13:15:43 +0000

    Tuesday class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Data types
+        int x = 5;
+        char c = 'c'; //initatilization
+        double d = 5.5;
+        boolean b = false;
+        int w ; //declarion
+        w =66;
+        int infractedStudents;//camelCasing
+        final int CONNY =5; //constants are all caps
+        w-=3 ; 
+        System.out.println(w);
+
+        //comparisions
+        boolean boo = false;
+        System.out.println(!boo !=false);
+
+        //conditonal
+        if(!boo) System.out.println("boo passed");
+        // == is a boolean = is and assignment
+
+        // SHORTCIRCUT
+        int divisor = 0
+        if( !=0 && 5/divisor >1 || 0) System.out.println("omg");
+        
     }
 }
